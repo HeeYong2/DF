e6cf85365cd876f3c6333367ed64e08a5a3cd144
HeeYong2 <fire126@naver.com>
2015-12-02 10:11:15 +0900

    setup.py & startgame.py 수정

--- 1945Striker/startgame.py
+++ 1945Striker/startgame.py
@@ -1,5 +1,13 @@
 __author__ = 'HeeYong'
 
+import platform
+import os
+
+if platform.architecture()[0] == "32bit":
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86"
+else:
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
+
 import game_framework
 import start_state
 
